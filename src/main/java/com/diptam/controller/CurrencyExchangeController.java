cb13bec66a85768a7f26e363c71a55c46f73b065
diptamsarkar@gmail.com <dipgit17>
2020-02-01 12:24:26 -0600

    Added Custom exception handler

--- src/main/java/com/diptam/controller/CurrencyExchangeController.java
+++ src/main/java/com/diptam/controller/CurrencyExchangeController.java
@@ -1,13 +1,17 @@
 package com.diptam.controller;
 
+import com.diptam.exception.ApiRequestException;
 import com.diptam.service.CurrencyExchangeService;
 import com.diptam.model.CurrencyExchange;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @Slf4j
@@ -17,8 +21,13 @@ public class CurrencyExchangeController {
     private CurrencyExchangeService service;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public ResponseEntity<CurrencyExchange> getExchange(@PathVariable String from, @PathVariable String to){
+    public ResponseEntity<CurrencyExchange> getExchange(@PathVariable String from, @PathVariable String to) throws ApiRequestException {
         log.info(from +" : "+ to);
-        return ResponseEntity.ok(service.findByCurrencyFromAndCurrencyTo(from, to).orElse(new CurrencyExchange()));
+        Optional<CurrencyExchange> body = service.findByCurrencyFromAndCurrencyTo(from, to);
+        if(body.isPresent()){
+            return ResponseEntity.ok(body.get());
+        }else{
+           throw new ApiRequestException("No Data Found");
+        }
     }
 }
