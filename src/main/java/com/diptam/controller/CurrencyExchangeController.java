b0afc80041e934ae0a092bf3465fa813eca1a5c8
diptamsarkar@gmail.com <dipgit17>
2020-02-02 22:09:00 -0600

    Finalised Code - Not including Zipkin

--- src/main/java/com/diptam/controller/CurrencyExchangeController.java
+++ src/main/java/com/diptam/controller/CurrencyExchangeController.java
@@ -5,6 +5,7 @@ import com.diptam.service.CurrencyExchangeService;
 import com.diptam.model.CurrencyExchange;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +21,13 @@ public class CurrencyExchangeController {
     @Autowired
     private CurrencyExchangeService service;
 
+    @Autowired
+   Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ResponseEntity<CurrencyExchange> getExchange(@PathVariable String from, @PathVariable String to) throws ApiRequestException {
-        log.info(from +" : "+ to);
+        String port = environment.getProperty("local.server.port");
+        log.info("Request received : From Currency - "+from+" : To Currency - "+to+"  >>> Server Port : "+port);
         Optional<CurrencyExchange> body = service.findByCurrencyFromAndCurrencyTo(from, to);
         if(body.isPresent()){
             return ResponseEntity.ok(body.get());
